83ab28c90677a9b1be43be512a0c230f16001408
dariofusco <fdario@hotmail.it>
2024-05-21 15:42:23 +0200

    crea server

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const http = require("http");
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || 'localhost';
+
+http
+    .createServer(function (req, res) {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        res.end("Hello World!");
+    })
+    .listen(port, host, () => {
+        const serverUrl = `http://${host}:${port}`
+        console.log(`Server avviato su ${serverUrl}`);
+    });
