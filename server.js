8bb22b09a3811daab71676f8230642b289df41ce
dariofusco <fdario@hotmail.it>
2024-05-21 15:57:19 +0200

    configura variabile da stampare in html

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const http = require("http");
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || 'localhost';
+const text = process.env.TEXT;
 
 http
     .createServer(function (req, res) {
         res.writeHead(200, {
             "Content-Type": "text/html"
         });
-        res.end("Hello World!");
+        res.end(text);
     })
     .listen(port, host, () => {
         const serverUrl = `http://${host}:${port}`
